58d0be879d8bc5ede24550ff07f2ae6b03a5d162
hlmtssdyh <77870339+hlmtssdyh@users.noreply.github.com>
2021-01-26 18:27:13 +0700

    Delete Makanan.vb

--- Makanan.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Makanan
-    Private Sub reload()
-
-    End Sub
-
-    Private Sub txtNamaMakanan_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtNamaMakanan.TextChanged
-
-    End Sub
-End Class
